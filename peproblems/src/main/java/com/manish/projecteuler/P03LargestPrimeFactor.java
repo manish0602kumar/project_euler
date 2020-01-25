62e01bf43e6616923a5f7ecfbb9b750870c22fba
Manish Kumar <manish0602kumar@gmail.com>
2020-01-25 21:15:22 +0530

    Java docs added for problem 3

--- peproblems/src/main/java/com/manish/projecteuler/P03LargestPrimeFactor.java
+++ peproblems/src/main/java/com/manish/projecteuler/P03LargestPrimeFactor.java
@@ -5,8 +5,6 @@
  */
 package com.manish.projecteuler;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -20,25 +18,42 @@ public class P03LargestPrimeFactor {
 
     public static final Logger log = Logger.getLogger(P03LargestPrimeFactor.class.getName());
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         largestPF(600851475143L);
-
     }
 
-    public static long largestPF(Long divendNumber) {
-        long num = divendNumber;
+    /**
+     * This Function calculates the largest Prime factor
+     *
+     * @param inputNumber
+     * @return
+     */
+    public static long largestPF(long inputNumber) {
+        long dividendNumber = inputNumber;
+        // Intially we are consider the largest Prime factor to be 1
         int largestPFNumber = 1;
-        for (int divisor = 2; divisor <= divendNumber  ; divisor++) {
+        for (int divisor = 2; divisor <= dividendNumber; divisor++) {
 
-            while (divendNumber % divisor == 0) {
-                System.out.println("Prime Factor of " + num + " is " + divisor);
+            /* checks whether  divisor(i.e loop varible)is a factor of  dividendNumber(i.e inputNumber)
+             if yes than make quotient the new divident and add divisor in the list of prime factor
+             else increase the divisor and try again  */
+            while (dividendNumber % divisor == 0) {
+                System.out.println("Prime Factor of " + inputNumber + " is " + divisor);
+
+                //if this factor of the input number is greater than previously stored largest prime factor than interchange.
                 if (largestPFNumber < divisor) {
                     largestPFNumber = divisor;
                 }
-                divendNumber = divendNumber / divisor;
+
+                // quotient will be the new dividend
+                dividendNumber = dividendNumber / divisor;
             }
         }
-        System.out.println("Largest Prime Factor of " + num + " is " + largestPFNumber);
+        System.out.println("Largest Prime Factor of " + inputNumber + " is " + largestPFNumber);
         return largestPFNumber;
     }
 
